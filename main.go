94849b08acffe88278ac4b58177796a6013ea42a
Bram Veerman <bram.veerman@gmail.com>
2024-08-09 12:48:32 -0700

    is able to run

--- main.go
+++ main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+	"io"
+	"log"
 	"os"
+	"os/exec"
 	"strings"
+	"sync"
+	"time"
 )
 
 type process struct {
-	shortname   string
-	command     string
-	description string
-	output      string
+	shortname    string
+	command      string
+	description  string
+	output       string
+	cmd          *exec.Cmd
+	lastActivity time.Time
 }
 
 type tab struct {
@@ -24,14 +32,16 @@ type model struct {
 	Tabs       []tab
 	activeTab  int
 	windowSize []int
-	processes  []process
+	processes  []*process
+	mutex      sync.Mutex
 }
 
 func (m model) Init() tea.Cmd {
-	return nil
+	log.Println("Initializing model")
+	return tea.Batch(m.startAllProcesses()...)
 }
 
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch keypress := msg.String(); keypress {
@@ -45,8 +55,17 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			return m, nil
 		}
 
+	case processStartedMsg:
+		log.Printf("Process started: %s\n", msg.process.shortname)
+		return m, m.updateNotifications()
+
+	case processErrorMsg:
+		log.Printf("Error starting process %s: %v\n", msg.process.shortname, msg.err)
+		return m, nil
+
 	case tea.WindowSizeMsg:
 		m.windowSize = []int{msg.Width, msg.Height}
+		return m, nil
 	}
 
 	return m, nil
@@ -61,10 +80,10 @@ func tabBorderWithBottom(left, middle, right string) lipgloss.Border {
 }
 
 //goland:noinspection ALL
-func (m model) View() string {
+func (m *model) View() string {
 	doc := strings.Builder{}
 
-	tabs := RenderTabs(m)
+	tabs := RenderTabs(*m)
 	doc.WriteString(tabs)
 	doc.WriteString("\n")
 
@@ -95,60 +114,140 @@ func (m model) View() string {
 		doc.WriteString(windowStyle.Padding(0, 1, 1, 1).Align(lipgloss.Top, lipgloss.Left).Render(windowContent))
 
 	} else {
-		doc.WriteString(windowStyle.Render("Not implemented yet"))
+		doc.WriteString(windowStyle.Render("No process selected"))
 	}
 
 	return DocStyle.Render(doc.String())
 }
 
+func (m *model) startProcess(p *process) tea.Cmd {
+	return func() tea.Msg {
+		log.Printf("Starting process: %s\n", p.shortname)
+		args := strings.Fields(p.command)
+		cmd := exec.Command(args[0], args[1:]...)
+		p.cmd = cmd
+
+		stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			return processErrorMsg{process: p, err: err}
+		}
+
+		stderr, err := cmd.StderrPipe()
+		if err != nil {
+			return processErrorMsg{process: p, err: err}
+		}
+
+		if err := cmd.Start(); err != nil {
+			return processErrorMsg{process: p, err: err}
+		}
+
+		go m.handleOutput(p, io.MultiReader(stdout, stderr))
+
+		return processStartedMsg{process: p}
+	}
+}
+
+func (m *model) startAllProcesses() []tea.Cmd {
+	cmds := make([]tea.Cmd, len(m.processes))
+	for i, p := range m.processes {
+		cmds[i] = m.startProcess(p)
+	}
+	return cmds
+}
+
+func (m *model) handleOutput(p *process, reader io.Reader) {
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		line := scanner.Text()
+		m.mutex.Lock()
+		p.output += line + "\n"
+		p.lastActivity = time.Now()
+		log.Printf("Process %s output: %s\n", p.shortname, line)
+		m.mutex.Unlock()
+	}
+	if err := scanner.Err(); err != nil {
+		log.Printf("Error reading output from process %s: %v\n", p.shortname, err)
+	}
+}
+
+func (m *model) updateNotifications() tea.Cmd {
+	return func() tea.Msg {
+		m.mutex.Lock()
+		defer m.mutex.Unlock()
+		for i, t := range m.Tabs {
+			if i == 0 {
+				continue // Skip the "all" tab
+			}
+			for _, p := range m.processes {
+				if p.shortname == t.name {
+					m.Tabs[i].notifications = time.Since(p.lastActivity) < time.Minute
+					break
+				}
+			}
+		}
+		return nil
+	}
+}
+
+type processStartedMsg struct {
+	process *process
+}
+
+type processErrorMsg struct {
+	process *process
+	err     error
+}
+
 func main() {
-	//tabs := []string{"Lip Gloss", "Blush", "Eye Shadow", "Mascara", "Foundation"}
-	//tabContent := []string{"Lip Gloss Tab", "Blush Tab", "Eye Shadow Tab", "Mascara Tab", "Foundation Tab"}
-	processes := []process{
+	// Set up logging to a file
+	logFile, err := os.Create("debug.log")
+	if err != nil {
+		fmt.Printf("Error creating log file: %v\n", err)
+		return
+	}
+	defer logFile.Close()
+	log.SetOutput(logFile)
+
+	log.Println("Starting application")
+
+	processes := []*process{
 		{
-			shortname:   "fronend",
-			command:     "yarn start",
+			shortname:   "frontend",
+			command:     "echo 'Frontend starting' && sleep 2 && echo 'Frontend ready'",
 			description: "Start the frontend server",
-			output:      "bunch of frontend output here",
+			output:      "",
 		},
 		{
 			shortname:   "mail",
-			command:     "open http://localhost:8025 && mailhog",
-			description: "Start the mailhog server and open the web interface",
-			output:      "bunch of mailhog output here",
+			command:     "echo 'Mailhog starting' && sleep 3 && echo 'Mailhog ready'",
+			description: "Start the mailhog server",
+			output:      "",
 		},
-		{
-			shortname:   "stripe",
-			command:     "yarn stripe:listen",
-			description: "Start the stripe webhook listener",
-			output:      "bunch of stripe output here",
-		},
-		{
-			shortname:   "queue",
-			command:     "php artisan queue:work",
-			description: "Start the queue worker",
-			output:      "bunch of queue output here",
-		},
-		{
-			shortname:   "schedule",
-			command:     "php artisan schedule:work",
-			description: "Start the scheduler",
-			output:      "bunch of schedule output here",
-		},
+		// ... other processes ...
 	}
 
 	tabs := generateTabs(processes)
 
-	m := model{
+	m := &model{
 		Tabs:       tabs,
 		processes:  processes,
 		activeTab:  0,
 		windowSize: []int{0, 0},
 	}
 
-	if _, err := tea.NewProgram(m, tea.WithAltScreen()).Run(); err != nil {
-		fmt.Println("Error running program:", err)
-		os.Exit(1)
+	p := tea.NewProgram(m, tea.WithAltScreen())
+
+	go func() {
+		ticker := time.NewTicker(10 * time.Second)
+		for range ticker.C {
+			p.Send(m.updateNotifications()())
+		}
+	}()
+
+	if _, err := p.Run(); err != nil {
+		log.Printf("Error running program: %v", err)
+		fmt.Printf("Error running program: %v", err)
+		return
 	}
 }
 
@@ -160,14 +259,14 @@ func (m model) getActiveProcess() *process {
 	// find by name
 	for _, p := range m.processes {
 		if p.shortname == m.getActiveTab().name {
-			return &p
+			return p
 		}
 	}
 
 	return nil
 }
 
-func generateTabs(processes []process) []tab {
+func generateTabs(processes []*process) []tab {
 	var tabs []tab
 
 	tabs = append(tabs, tab{
@@ -175,10 +274,10 @@ func generateTabs(processes []process) []tab {
 		notifications: false,
 	})
 
-	for idx, p := range processes {
+	for _, p := range processes {
 		tabs = append(tabs, tab{
 			name:          p.shortname,
-			notifications: idx == 3,
+			notifications: false,
 		})
 	}
 	return tabs
