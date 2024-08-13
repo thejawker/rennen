15f975db710a01c3cfa20d8f3918e5e9cdb051bf
Bram Veerman <bram.veerman@gmail.com>
2024-08-12 20:16:06 -0700

    f f f f fre free free stylaaa

--- internal/ui/overview.go
+++ internal/ui/overview.go
@@ -2,8 +2,10 @@ package ui
 
 import (
 	"fmt"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/thejawker/rennen/internal/types"
 	"github.com/thejawker/rennen/internal/utils"
+	"strings"
 	"time"
 )
 
@@ -20,33 +22,64 @@ func renderOverview(m types.ViewModelProvider, maxLines int) string {
 	//content := lipgloss.NewStyle().Height(maxLines - 1).Render(count)
 	windowWidth := m.GetViewModel().WindowSize.Width - windowStyle.GetHorizontalFrameSize() - 2
 	commandList := renderCommandList(m, windowWidth)
-	processTable := renderProcessTable(m, maxLines-1, windowWidth)
+	commandLines := strings.Split(commandList, "\n")
+	processTable := renderProcessTable(m, maxLines-len(commandLines)-2, windowWidth)
 
 	hint := hintStyle.Width(windowWidth).Render("←/→ tabs, (q)uit all")
 
-	return fmt.Sprintf("%s\n%s\n%s", commandList, processTable, hint)
+	return fmt.Sprintf("%s\n\n%s\n%s", commandList, processTable, hint)
 }
 
-func renderCommandList(m types.ViewModelProvider, width int) interface{} {
-	list := "Shortcuts:\n"
+func renderCommandList(m types.ViewModelProvider, width int) string {
+	list := "shortcuts:\n"
+
+	listStyle := lipgloss.NewStyle().
+		Border(lipgloss.ThickBorder()).
+		BorderLeft(true).
+		BorderRight(false).
+		BorderTop(false).
+		BorderBottom(false).
+		PaddingLeft(1).
+		BorderForeground(lipgloss.Color("253"))
+
+	if len(m.GetViewModel().Commands) == 0 {
+		list += lipgloss.NewStyle().
+			Italic(true).
+			Faint(true).
+			PaddingTop(1).
+			Render("consider adding a command so you can easily ren it\n")
+		return listStyle.Render(list)
+	}
+
 	for i, cmd := range m.GetViewModel().Commands {
-		// use a space to separate the command from the description
-		// to show selected you do > before the command
+		style := lipgloss.NewStyle()
+
 		prefix := " "
 		if i == m.GetViewModel().SelectedCommand {
+			style = style.Foreground(lipgloss.Color("#555"))
 			prefix = "›"
 		}
+
+		// pink
+		prefix = lipgloss.NewStyle().
+			PaddingRight(1).
+			PaddingRight(1).
+			Foreground(lipgloss.Color("#ff00ff")).
+			Render(prefix)
 
 		status := "•"
 		// we check whether the command is running or not and if it has been active within last 10 seconds
 		if cmd.LastActivity.Add(3 * time.Second).After(time.Now()) {
+			style = style.Foreground(lipgloss.Color("#15803d"))
 			status = "✓"
 		}
 
-		list += fmt.Sprintf(" %s %s %s\n", prefix, status, cmd.Shortname)
+		list += prefix
+		list += style.Render(fmt.Sprintf("%s %s", status, cmd.Shortname))
+		list += "\n"
 	}
 
-	return list
+	return listStyle.Render(list)
 }
 
 func renderProcessTable(m types.ViewModelProvider, height, width int) string {
@@ -70,5 +103,7 @@ func renderProcessTable(m types.ViewModelProvider, height, width int) string {
 		table.AddRow([]string{proc.Shortname, proc.GetLastNonEmptyLine(), status})
 	}
 
-	return table.Render()
+	return lipgloss.NewStyle().
+		Height(height).
+		Render(table.Render())
 }
